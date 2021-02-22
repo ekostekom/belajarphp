57d28f1876a92a5ef5b7dd03df165c999f716fc6
eycoss_universitas_stekom <79434923+ekostekom@users.noreply.github.com>
2021-02-22 11:38:00 +0700

    Add files via upload

---
+++ operator.php
@@ -0,0 +1,32 @@
+<?php
+$a=5;
+$b = 2 ;
+
+// PENJUMLAHAN
+$c = $a + $b;
+echo "$a+$b=$c";
+echo "<hr>";
+// pengurangan
+$c = $a - $b;
+echo "$a-$b=$c";
+echo "<hr>";
+// Perkalian
+$c = $a * $b;
+echo "$a*$b=$c";
+echo "<hr>";
+// Pembagian
+$c = $a / $b;
+echo "$a/$b=$c";
+echo "<hr>";
+// Sisabagi
+$c = $a % $b;
+echo "$a%$b=$c";
+echo "<hr>";
+// Pangkat
+$c = $a  **  $b;
+echo "$a ** $b=$c";
+echo "<hr>";
+
+
+
+ ?>
